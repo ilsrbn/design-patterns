a86a076bd68b1098d00b903f04622cc3bd04e6d4
xavi <serbini271@gmail.com>
2021-06-21 20:28:49 +0300

    composite updated

--- 2structural/3composite/composite.ts
+++ 2structural/3composite/composite.ts
@@ -1,58 +1,61 @@
-abstract class Component {
-    parent: Component
-
-	setParent(parent: Component) {
-        this.parent = parent
-    }
-
-    getParent(): Component {
-        return this.parent
-    }
-
-    add(component: Component): void { }
-
-    remove(component: Component): void { }
-
-    isComposite(): boolean {
-        return false
-    }
-
-    abstract operation(): string;
+interface Food {
+	eat(): void
 }
 
-class Leaf extends Component {
-    operation(): string {
-        return 'Leaf'
-    }
+class FoodContainer implements Food {
+	private children: Array<Food> = []
+	add(food: Food): void {
+		this.children.push(food)
+	}
+	remove(food: Food): void {
+		this.children.splice(this.children.indexOf(food), 1)
+	}
+
+	eat(): void {
+
+		for (var i = this.children.length - 1; i >= 0; i--) {
+			this.children[i].eat()
+		}
+	}
 }
 
-class Composite extends Component {
-    children: Component[] = [];
+class Meal implements Food {
+	name: string
 
-    add(component: Component): void {
-        this.children.push(component)
-        component.setParent(this)
-    }
+	constructor(name: string) {
+		this.name = name
+	}
 
-    remove(component: Component): void {
-        const componentIndex = this.children.indexOf(component)
-        this.children.splice(componentIndex, 1)
-
-        component.setParent(null)
-    }
-
-    isComposite(): boolean {
-        return true
-    }
-
-    operation(): string {
-        const results = [];
-        for (const child of this.children) {
-            results.push(child.operation())
-        }
-
-        return `Branch(${results.join('+')})`
-    }
+	eat(): void {
+		console.log(this.name)
+	}
 }
 
+let meal1 = new Meal("рис")
+let meal2 = new Meal("курага")
+let meal3 = new Meal("бургер")
+let meal4 = new Meal("кола")
+let meal5 = new Meal("мясо")
+let meal6 = new Meal("творог")
 
+let foodContainer2 = new FoodContainer()
+
+foodContainer2.add(meal1)
+foodContainer2.add(meal2)
+foodContainer2.add(meal3)
+
+let foodContainer3 = new FoodContainer()
+
+foodContainer3.add(meal4)
+foodContainer3.add(meal5)
+foodContainer3.add(meal6)
+
+foodContainer2.remove(meal3)
+foodContainer3.remove(meal4)
+
+let foodContainer1 = new FoodContainer()
+
+foodContainer1.add(foodContainer2)
+foodContainer1.add(foodContainer3)
+
+foodContainer1.eat()
