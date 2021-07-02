ada251d1f2f2e700bd5e6d46eb36099809e69ac8
xavi <serbini271@gmail.com>
2021-07-02 17:38:09 +0300

    added "flyweight" patterns code

--- 2structural/6flyweight/flyweight.ts
+++ 2structural/6flyweight/flyweight.ts
@@ -1,19 +1,32 @@
-class CoffeeBrand {
-	private brand: string
-	private price: number
-
-	constructor(brand: string, cost: number) {
-		this.brand = brand
-		this.cost = cost
+class ChairType {
+	material: string
+	constructor(material: string = 'wood') {
+		this.material = material
 	}
-
-	getCost(weight: number): number {
-		return weight * this.price
+	setWeight(weight: number) {
+		console.log(`\nСоздан стул.\nМатериал: ${this.material}.\nВес: ${weight}.\n`)
 	}
 }
 
-class CoffeeShop {
-	private coffeeBrands: {[key: string]: CoffeeBrand} = <any>{}
-
-	constructor()
+class ChairFactory {
+	chairTypes: Array<ChairType> = []
+	getChairType(material: string = 'wood') {
+		for (var i = this.chairTypes.length - 1; i >= 0; i--) {
+			if (this.chairTypes[i].material == material) {
+				console.log('\nВозврат закешиорованного объекта.')
+				console.log(this.chairTypes[i])
+				return this.chairTypes[i]
+			}
+		}
+		console.log('\nСоздание нового и добавление в кеш объекта.\n')
+		this.chairTypes.push(new ChairType(material))
+	}
 }
+
+let factory = new ChairFactory
+
+factory.getChairType()
+factory.getChairType('plastic')
+factory.getChairType('wood')
+factory.getChairType('plastic')
+
